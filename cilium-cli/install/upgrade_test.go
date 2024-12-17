441fb3f0ce39a2ba7846630f4fd2e0a9120d7a87
Marco Franssen <marco.franssen@gmail.com>
2024-12-17 20:42:57 +0100

    cli: Add support for helm --max-history command line option
    By using this flag and same default as the upstream Helm project we can
    give users same behavior. Without this people will end up with hundreds
    of helm releases on their clusters. Using this option we will limit them
    by default to 10 releases and allow users to choose for a longer or
    shorter history.
    
    Signed-off-by: Marco Franssen <marco.franssen@gmail.com>

---
+++ cilium-cli/install/upgrade_test.go
@@ -0,0 +1,19 @@
+package install
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMaxHistory(t *testing.T) {
+	assert := assert.New(t)
+
+	p := Parameters{
+		HelmMaxHistory: 15,
+	}
+
+	installer, err := NewK8sInstaller(nil, p)
+	assert.NoError(err)
+	assert.Equal(15, installer.params.HelmMaxHistory)
+}
