cilium/cilium

commit 1b2bed77dad58e1c98d801762b79262a40a3bf2c

cilium, bpf: Do not extend lb{4,6}_service struct via defines
We cannot extend structs based on defines to enlarge them as this compromises
the ongoing work to achieve a clang-free Cilium runtime image, or at least
makes it significantly harder. Therefore use the upper 8 bits of the affinity
timeout field in the service master entry.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
Link: https://github.com/cilium/cilium/pull/35735#discussion_r1853845710

pkg/maps/lbmap/ipv4.go
@@ -70,26 +70,14 @@ func initSVC(params InitParams) {
 	RevNatMapMaxEntries = params.RevNatMapMaxEntries
 
 	if params.IPv4 {
-		if params.PerSvcLbEnabled {
-			Service4MapV2 = bpf.NewMap(Service4MapV2Name,
-				ebpf.Hash,
-				&Service4Key{},
-				&Service4ExtendedValue{},
-				ServiceMapMaxEntries,
-				0,
-			).WithCache().WithPressureMetric().
-				WithEvents(option.Config.GetEventBufferConfig(Service4MapV2Name))
-		} else {
-			Service4MapV2 = bpf.NewMap(Service4MapV2Name,
-				ebpf.Hash,
-				&Service4Key{},
-				&Service4Value{},
-				ServiceMapMaxEntries,
-				0,
-			).WithCache().WithPressureMetric().
-				WithEvents(option.Config.GetEventBufferConfig(Service4MapV2Name))
-		}
-
+		Service4MapV2 = bpf.NewMap(Service4MapV2Name,
+			ebpf.Hash,
+			&Service4Key{},
+			&Service4Value{},
+			ServiceMapMaxEntries,
+			0,
+		).WithCache().WithPressureMetric().
+			WithEvents(option.Config.GetEventBufferConfig(Service4MapV2Name))
 		Backend4Map = bpf.NewMap(Backend4MapName,
 			ebpf.Hash,
 			&Backend4Key{},
@@ -125,27 +113,14 @@ func initSVC(params InitParams) {
 	}
 
 	if params.IPv6 {
-		if params.PerSvcLbEnabled {
-			Service6MapV2 = bpf.NewMap(Service6MapV2Name,
-				ebpf.Hash,
-				&Service6Key{},
-				&Service6ExtendedValue{},
-				ServiceMapMaxEntries,
-				0,
-			).WithCache().WithPressureMetric().
-				WithEvents(option.Config.GetEventBufferConfig(Service6MapV2Name))
-
-		} else {
-			Service6MapV2 = bpf.NewMap(Service6MapV2Name,
-				ebpf.Hash,
-				&Service6Key{},
-				&Service6Value{},
-				ServiceMapMaxEntries,
-				0,
-			).WithCache().WithPressureMetric().
-				WithEvents(option.Config.GetEventBufferConfig(Service6MapV2Name))
-
-		}
+		Service6MapV2 = bpf.NewMap(Service6MapV2Name,
+			ebpf.Hash,
+			&Service6Key{},
+			&Service6Value{},
+			ServiceMapMaxEntries,
+			0,
+		).WithCache().WithPressureMetric().
+			WithEvents(option.Config.GetEventBufferConfig(Service6MapV2Name))
 		Backend6Map = bpf.NewMap(Backend6MapName,
 			ebpf.Hash,
 			&Backend6Key{},
@@ -186,7 +161,6 @@ var _ RevNatKey = (*RevNat4Key)(nil)
 var _ RevNatValue = (*RevNat4Value)(nil)
 var _ ServiceKey = (*Service4Key)(nil)
 var _ ServiceValue = (*Service4Value)(nil)
-var _ ServiceValue = (*Service4ExtendedValue)(nil)
 var _ BackendKey = (*Backend4Key)(nil)
 var _ BackendKey = (*Backend4KeyV3)(nil)
 var _ BackendValue = (*Backend4Value)(nil)
@@ -353,8 +327,9 @@ func (s *Service4Value) GetFlags() uint16 {
 
 func (s *Service4Value) SetSessionAffinityTimeoutSec(t uint32) {
 	// Go doesn't support union types, so we use BackendID to access the
-	// lb4_service.affinity_timeout field
-	s.BackendID = t
+	// lb4_service.affinity_timeout field. Also, for the master entry the
+	// LB algorithm was set beforehand, so we need to binary OR here.
+	s.BackendID |= t
 }
 
 func (s *Service4Value) SetL7LBProxyPort(port uint16) {
@@ -371,10 +346,11 @@ func (s *Service4Value) GetBackendID() loadbalancer.BackendID {
 }
 
 func (s *Service4Value) GetLbAlg() uint8 {
-	return 0
+	return uint8(uint32(s.BackendID) >> 24)
 }
 
 func (s *Service4Value) SetLbAlg(lb uint8) {
+	s.BackendID = uint32(lb) << 24
 }
 
 func (s *Service4Value) ToNetwork() ServiceValue {
@@ -385,41 +361,6 @@ func (s *Service4Value) ToNetwork() ServiceValue {
 
 // ToHost converts Service4Value to host byte order.
 func (s *Service4Value) ToHost() ServiceValue {
-	h := *s
-	h.RevNat = byteorder.NetworkToHost16(h.RevNat)
-	return &h
-}
-
-// Service4ExtendedValue must match 'struct lb4_service' in "bpf/lib/common.h" with LB_SELECTION_PER_SERVICE set.
-type Service4ExtendedValue struct {
-	Service4Value
-	LbAlg uint8    `align:"lb_alg"`
-	Pad   [3]uint8 `align:"pad"`
-}
-
-func (s *Service4ExtendedValue) New() bpf.MapValue { return &Service4ExtendedValue{} }
-
-func (s *Service4ExtendedValue) String() string {
-	sHost := s.ToHost().(*Service4ExtendedValue)
-	return fmt.Sprintf("%d %d[%d] (%d) [0x%x 0x%x] %d", sHost.BackendID, sHost.Count, sHost.QCount, sHost.RevNat, sHost.Flags, sHost.Flags2, s.LbAlg)
-}
-
-func (s *Service4ExtendedValue) GetLbAlg() uint8 {
-	return s.LbAlg
-}
-
-func (s *Service4ExtendedValue) SetLbAlg(lb uint8) {
-	s.LbAlg = lb
-}
-
-func (s *Service4ExtendedValue) ToNetwork() ServiceValue {
-	n := *s
-	n.RevNat = byteorder.HostToNetwork16(n.RevNat)
-	return &n
-}
-
-// ToHost converts Service4ExtendedValue to host byte order.
-func (s *Service4ExtendedValue) ToHost() ServiceValue {
 	h := *s
 	h.RevNat = byteorder.NetworkToHost16(h.RevNat)
 	return &h

pkg/maps/lbmap/ipv6.go
@@ -67,7 +67,6 @@ var _ RevNatKey = (*RevNat6Key)(nil)
 var _ RevNatValue = (*RevNat6Value)(nil)
 var _ ServiceKey = (*Service6Key)(nil)
 var _ ServiceValue = (*Service6Value)(nil)
-var _ ServiceValue = (*Service6ExtendedValue)(nil)
 var _ BackendKey = (*Backend6Key)(nil)
 var _ BackendValue = (*Backend6Value)(nil)
 var _ Backend = (*Backend6)(nil)
@@ -220,9 +219,12 @@ func (s *Service6Value) SetFlags(flags uint16) {
 	s.Flags2 = uint8(flags >> 8)
 }
 
-func (s *Service6Value) GetLbAlg() uint8 { return 0 }
+func (s *Service6Value) GetLbAlg() uint8 {
+	return uint8(uint32(s.BackendID) >> 24)
+}
 
 func (s *Service6Value) SetLbAlg(lb uint8) {
+	s.BackendID = uint32(lb) << 24
 }
 
 func (s *Service6Value) GetFlags() uint16 {
@@ -231,7 +233,7 @@ func (s *Service6Value) GetFlags() uint16 {
 
 func (s *Service6Value) SetSessionAffinityTimeoutSec(t uint32) {
 	// See (* Service4Value).SetSessionAffinityTimeoutSec() for comment
-	s.BackendID = t
+	s.BackendID |= t
 }
 
 func (s *Service6Value) SetL7LBProxyPort(port uint16) {
@@ -256,41 +258,6 @@ func (s *Service6Value) ToNetwork() ServiceValue {
 
 // ToHost converts Service6Value to host byte order.
 func (s *Service6Value) ToHost() ServiceValue {
-	h := *s
-	h.RevNat = byteorder.NetworkToHost16(h.RevNat)
-	return &h
-}
-
-// Service6ExtendedValue must match 'struct lb6_service' in "bpf/lib/common.h".
-type Service6ExtendedValue struct {
-	Service6Value
-	LbAlg uint8    `align:"lb_alg"`
-	Pad   [3]uint8 `align:"pad"`
-}
-
-func (s *Service6ExtendedValue) String() string {
-	sHost := s.ToHost().(*Service6ExtendedValue)
-	return fmt.Sprintf("%d %d[%d] (%d) [0x%x 0x%x] %d", sHost.BackendID, sHost.Count, sHost.QCount, sHost.RevNat, sHost.Flags, sHost.Flags2, s.LbAlg)
-}
-
-func (s *Service6ExtendedValue) GetLbAlg() uint8 {
-	return s.LbAlg
-}
-
-func (s *Service6ExtendedValue) SetLbAlg(lb uint8) {
-	s.LbAlg = lb
-}
-
-func (s *Service6ExtendedValue) New() bpf.MapValue { return &Service6ExtendedValue{} }
-
-func (s *Service6ExtendedValue) ToNetwork() ServiceValue {
-	n := *s
-	n.RevNat = byteorder.HostToNetwork16(n.RevNat)
-	return &n
-}
-
-// ToHost converts Service6Value to host byte order.
-func (s *Service6ExtendedValue) ToHost() ServiceValue {
 	h := *s
 	h.RevNat = byteorder.NetworkToHost16(h.RevNat)
 	return &h

bpf/Makefile
@@ -35,7 +35,7 @@ LB_OPTIONS = \
 	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER: \
 	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER:-DENABLE_SRC_RANGE_CHECK: \
 	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER:-DENABLE_SRC_RANGE_CHECK:-DLB_SELECTION:-DLB_SELECTION_MAGLEV: \
-	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER:-DENABLE_SRC_RANGE_CHECK:-DLB_SELECTION:-DLB_SELECTION_PER_SERVICE:-DLB_SELECTION_MAGLEV \
+	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER:-DENABLE_SRC_RANGE_CHECK:-DLB_SELECTION:-DLB_SELECTION_RANDOM: \
 	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER:-DENABLE_SRC_RANGE_CHECK:-DLB_SELECTION:-DLB_SELECTION_MAGLEV:-DENABLE_SOCKET_LB_HOST_ONLY: \
 	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER:-DENABLE_SRC_RANGE_CHECK:-DLB_SELECTION:-DLB_SELECTION_MAGLEV:-DENABLE_SOCKET_LB_HOST_ONLY:-DENABLE_L7_LB:-DENABLE_SCTP: \
 	-DENABLE_IPV6:-DENCAP_IFINDEX:-DTUNNEL_MODE:-DENABLE_NODEPORT:-DENABLE_NODEPORT_ACCELERATION:-DENABLE_SESSION_AFFINITY:-DENABLE_BANDWIDTH_MANAGER:-DENABLE_SRC_RANGE_CHECK:-DLB_SELECTION:-DLB_SELECTION_MAGLEV:-DENABLE_SOCKET_LB_HOST_ONLY:-DENABLE_L7_LB:-DENABLE_SCTP:-DENABLE_VTEP: \
@@ -64,12 +64,12 @@ endif
 
 ifndef MAX_LB_OPTIONS
 MAX_LB_OPTIONS = $(MAX_BASE_OPTIONS) -DENABLE_NAT_46X64=1 -DENABLE_NAT_46X64_GATEWAY=1 -DENCAP_IFINDEX=1 -DTUNNEL_MODE=1
-MAX_LB_OPTIONS += -DLB_SELECTION=1 -DLB_SELECTION_MAGLEV=1 -DENABLE_IPSEC=1
+MAX_LB_OPTIONS += -DLB_SELECTION_PER_SERVICE=1 -DLB_SELECTION_MAGLEV=2 -DLB_SELECTION_RANDOM=1 -DENABLE_IPSEC=1
 endif
 
 ifndef MAX_OVERLAY_OPTIONS
 MAX_OVERLAY_OPTIONS = $(MAX_BASE_OPTIONS) -DENCAP_IFINDEX=1 -DTUNNEL_MODE=1 -DENABLE_MULTICAST=1
-MAX_OVERLAY_OPTIONS += -DLB_SELECTION=1 -DLB_SELECTION_MAGLEV=1 -DENABLE_IPSEC=1
+MAX_OVERLAY_OPTIONS += -DLB_SELECTION_PER_SERVICE=1 -DLB_SELECTION_MAGLEV=2 -DLB_SELECTION_RANDOM=1 -DENABLE_IPSEC=1
 ifeq ($(KERNEL),netnext)
 MAX_OVERLAY_OPTIONS += -DENABLE_HIGH_SCALE_IPCACHE=1
 endif
@@ -139,7 +139,7 @@ XDP_OPTIONS = $(LB_OPTIONS) \
 
 ifndef MAX_XDP_OPTIONS
 MAX_XDP_OPTIONS = $(MAX_BASE_OPTIONS) -DENABLE_PREFILTER=1
-MAX_XDP_OPTIONS += -DLB_SELECTION=1 -DLB_SELECTION_MAGLEV=1
+MAX_XDP_OPTIONS += -DLB_SELECTION_PER_SERVICE=1 -DLB_SELECTION_MAGLEV=2 -DLB_SELECTION_RANDOM=1
 endif
 
 # The following option combinations are compile tested

bpf/lib/common.h
@@ -1010,29 +1010,19 @@ struct lb6_key {
 	__u8 pad[2];
 };
 
-/* See lb4_service comments */
+/* See lb4_service comments for all fields. */
 struct lb6_service {
 	union {
-		__u32 backend_id;	/* Backend ID in lb6_backends */
-		__u32 affinity_timeout;	/* In seconds, only for svc frontend */
-		__u32 l7_lb_proxy_port;	/* In host byte order, only when flags2 && SVC_FLAG_L7LOADBALANCER */
+		__u32 backend_id;
+		/* See lb4_service for storage internals. */
+		__u32 affinity_timeout;
+		__u32 l7_lb_proxy_port;
 	};
 	__u16 count;
 	__u16 rev_nat_index;
 	__u8 flags;
 	__u8 flags2;
-	/* For the service frontend, qcount denotes number of service backend
-	 * slots under quarantine (otherwise zero).
-	 */
 	__u16 qcount;
-#ifdef LB_SELECTION_PER_SERVICE
-    /* Load balancer algorithm
-     * 1 - random
-     * 2 - maglev
-     */
-    __u8 lb_algorithm;
-    __u8 pad[3];
-#endif
 };
 
 /* See lb4_backend comments */
@@ -1080,11 +1070,28 @@ struct lb4_key {
 	__u8 pad[2];
 };
 
+#define LB_ALGORITHM_SHIFT	24
+#define AFFINITY_TIMEOUT_MASK	((1 << LB_ALGORITHM_SHIFT) - 1)
+
 struct lb4_service {
 	union {
-		__u32 backend_id;	/* Backend ID in lb4_backends */
-		__u32 affinity_timeout;	/* In seconds, only for svc frontend */
-		__u32 l7_lb_proxy_port;	/* In host byte order, only when flags2 && SVC_FLAG_L7LOADBALANCER */
+		/* Non-master entry: backend ID in lb4_backends */
+		__u32 backend_id;
+		/* For master entry:
+		 * - Upper  8 bits: load balancer algorithm,
+		 *                  values:
+		 *                     1 - random
+		 *                     2 - maglev
+		 * - Lower 24 bits: timeout in seconds
+		 * Note: We don't use bitfield here given storage is
+		 * compiler implementation dependent and the map needs
+		 * to be populated from Go.
+		 */
+		__u32 affinity_timeout;
+		/* For master entry: proxy port in host byte order,
+		 * only when flags2 & SVC_FLAG_L7LOADBALANCER is set.
+		 */
+		__u32 l7_lb_proxy_port;
 	};
 	/* For the service frontend, count denotes number of service backend
 	 * slots (otherwise zero).
@@ -1097,14 +1104,6 @@ struct lb4_service {
 	 * slots under quarantine (otherwise zero).
 	 */
 	__u16 qcount;
-#ifdef LB_SELECTION_PER_SERVICE
-    /* Load balancer algorithm
-     * 1 - random
-     * 2 - maglev
-     */
-    __u8 lb_algorithm;
-    __u8 pad[3];
-#endif
 };
 
 struct lb4_backend {

bpf/lib/lb.h
@@ -19,6 +19,15 @@
 #ifdef ENABLE_IPV6
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, struct skip_lb6_key);
+	__type(value, __u8);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+	__uint(max_entries, CILIUM_LB_SKIP_MAP_MAX_ENTRIES);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+} LB6_SKIP_MAP __section_maps_btf;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
 	__type(key, __u16);
 	__type(value, struct lb6_reverse_nat);
 	__uint(pinning, LIBBPF_PIN_BY_NAME);
@@ -92,15 +101,6 @@ struct {
 	});
 } LB6_MAGLEV_MAP_OUTER __section_maps_btf;
 #endif /* LB_SELECTION == LB_SELECTION_MAGLEV */
-
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__type(key, struct skip_lb6_key);
-	__type(value, __u8);
-	__uint(pinning, LIBBPF_PIN_BY_NAME);
-	__uint(max_entries, CILIUM_LB_SKIP_MAP_MAX_ENTRIES);
-	__uint(map_flags, BPF_F_NO_PREALLOC);
-} LB6_SKIP_MAP __section_maps_btf;
 #endif /* ENABLE_IPV6 */
 
 #ifdef ENABLE_IPV4
@@ -171,7 +171,7 @@ struct {
 } LB4_HEALTH_MAP __section_maps_btf;
 #endif
 
-#if LB_SELECTION == LB_SELECTION_MAGLEV
+#if defined(LB_SELECTION_PER_SERVICE) || LB_SELECTION == LB_SELECTION_MAGLEV
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH_OF_MAPS);
 	__type(key, __u16);
@@ -732,16 +732,24 @@ lb6_select_backend_id_maglev(struct __ctx_buff *ctx __maybe_unused,
 #endif  /* defined(LB_SELECTION_PER_SERVICE) || LB_SELECTION == LB_SELECTION_RANDOM */
 
 #ifdef LB_SELECTION_PER_SERVICE
+static __always_inline __u32 lb6_algorithm(const struct lb6_service *svc)
+{
+	return svc->affinity_timeout >> LB_ALGORITHM_SHIFT;
+}
+
 static __always_inline __u32
-lb6_select_backend_id(struct __ctx_buff *ctx,
-		      struct lb6_key *key,
-		      const struct ipv6_ct_tuple *tuple __maybe_unused,
+lb6_select_backend_id(struct __ctx_buff *ctx, struct lb6_key *key,
+		      const struct ipv6_ct_tuple *tuple,
 		      const struct lb6_service *svc)
 {
-	if (svc->lb_algorithm == LB_SELECTION_MAGLEV)
+	switch (lb6_algorithm(svc)) {
+	case LB_SELECTION_MAGLEV:
 		return lb6_select_backend_id_maglev(ctx, key, tuple, svc);
-
-	return lb6_select_backend_id_random(ctx, key, tuple, svc);
+	case LB_SELECTION_RANDOM:
+		return lb6_select_backend_id_random(ctx, key, tuple, svc);
+	default:
+		return 0;
+	}
 }
 #elif LB_SELECTION == LB_SELECTION_RANDOM
 # define lb6_select_backend_id	lb6_select_backend_id_random
@@ -789,6 +797,11 @@ static __always_inline int lb6_xlate(struct __ctx_buff *ctx, __u8 nexthdr,
 }
 
 #ifdef ENABLE_SESSION_AFFINITY
+static __always_inline __u32 lb6_affinity_timeout(const struct lb6_service *svc)
+{
+	return svc->affinity_timeout & AFFINITY_TIMEOUT_MASK;
+}
+
 static __always_inline __u32
 __lb6_affinity_backend_id(const struct lb6_service *svc, bool netns_cookie,
 			  union lb6_affinity_client_id *id)
@@ -813,7 +826,7 @@ __lb6_affinity_backend_id(const struct lb6_service *svc, bool netns_cookie,
 		};
 
 		if (READ_ONCE(val->last_used) +
-		    bpf_sec_to_mono(svc->affinity_timeout) <= now) {
+		    bpf_sec_to_mono(lb6_affinity_timeout(svc)) <= now) {
 			map_delete_elem(&LB6_AFFINITY_MAP, &key);
 			return 0;
 		}
@@ -1436,16 +1449,24 @@ lb4_select_backend_id_maglev(struct __ctx_buff *ctx __maybe_unused,
 #endif /* LB_SELECTION_PER_SERVICE || LB_SELECTION == LB_SELECTION_MAGLEV */
 
 #ifdef LB_SELECTION_PER_SERVICE
+static __always_inline __u32 lb4_algorithm(const struct lb4_service *svc)
+{
+	return svc->affinity_timeout >> LB_ALGORITHM_SHIFT;
+}
+
 static __always_inline __u32
-lb4_select_backend_id(struct __ctx_buff *ctx,
-		      struct lb4_key *key,
-		      const struct ipv4_ct_tuple *tuple __maybe_unused,
+lb4_select_backend_id(struct __ctx_buff *ctx, struct lb4_key *key,
+		      const struct ipv4_ct_tuple *tuple,
 		      const struct lb4_service *svc)
 {
-	if (svc->lb_algorithm == LB_SELECTION_MAGLEV)
+	switch (lb4_algorithm(svc)) {
+	case LB_SELECTION_MAGLEV:
 		return lb4_select_backend_id_maglev(ctx, key, tuple, svc);
-
-	return lb4_select_backend_id_random(ctx, key, tuple, svc);
+	case LB_SELECTION_RANDOM:
+		return lb4_select_backend_id_random(ctx, key, tuple, svc);
+	default:
+		return 0;
+	}
 }
 #elif LB_SELECTION == LB_SELECTION_RANDOM
 # define lb4_select_backend_id	lb4_select_backend_id_random
@@ -1513,6 +1534,11 @@ lb4_xlate(struct __ctx_buff *ctx, __be32 *new_saddr __maybe_unused,
 }
 
 #ifdef ENABLE_SESSION_AFFINITY
+static __always_inline __u32 lb4_affinity_timeout(const struct lb4_service *svc)
+{
+	return svc->affinity_timeout & AFFINITY_TIMEOUT_MASK;
+}
+
 static __always_inline __u32
 __lb4_affinity_backend_id(const struct lb4_service *svc, bool netns_cookie,
 			  const union lb4_affinity_client_id *id)
@@ -1538,7 +1564,7 @@ __lb4_affinity_backend_id(const struct lb4_service *svc, bool netns_cookie,
 		 * Session is sticky for range [current, last_used + affinity_timeout)
 		 */
 		if (READ_ONCE(val->last_used) +
-		    bpf_sec_to_mono(svc->affinity_timeout) <= now) {
+		    bpf_sec_to_mono(lb4_affinity_timeout(svc)) <= now) {
 			map_delete_elem(&LB4_AFFINITY_MAP, &key);
 			return 0;
 		}

pkg/datapath/alignchecker/alignchecker.go
@@ -61,17 +61,10 @@ func CheckStructAlignments(path string) error {
 }
 
 func RegisterLbStructsToCheck(isPerSvcLb bool) {
-	if isPerSvcLb {
-		registerToCheck(map[string][]any{
-			"lb4_service": {lbmap.Service4ExtendedValue{}},
-			"lb6_service": {lbmap.Service6ExtendedValue{}},
-		})
-	} else {
-		registerToCheck(map[string][]any{
-			"lb4_service": {lbmap.Service4Value{}},
-			"lb6_service": {lbmap.Service6Value{}},
-		})
-	}
+	registerToCheck(map[string][]any{
+		"lb4_service": {lbmap.Service4Value{}},
+		"lb6_service": {lbmap.Service6Value{}},
+	})
 }
 
 func init() {

pkg/defaults/defaults.go
@@ -189,6 +189,10 @@ const (
 	// is considered failed
 	StatusCollectorFailureThreshold = 1 * time.Minute
 
+	// SessionAffinityTimeoutMaxFallback defines the maximum number of seconds
+	// for the session affinity timeout. See also lb{4,6}_affinity_timeout().
+	SessionAffinityTimeoutMaxFallback = 0xffffff
+
 	// EnableIPv4 is the default value for IPv4 enablement
 	EnableIPv4 = true
 

pkg/k8s/service.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/cidr"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ip"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	"github.com/cilium/cilium/pkg/k8s/utils"
@@ -290,6 +291,11 @@ func ParseService(svc *slim_corev1.Service, nodePortAddrs []netip.Addr) (Service
 		}
 		if svcInfo.SessionAffinityTimeoutSec == 0 {
 			svcInfo.SessionAffinityTimeoutSec = uint32(v1.DefaultClientIPServiceAffinitySeconds)
+		}
+		if svcInfo.SessionAffinityTimeoutSec > defaults.SessionAffinityTimeoutMaxFallback {
+			scopedLog.Warnf("Clamping maximum possible session affinity timeout from %d to %d seconds",
+				svcInfo.SessionAffinityTimeoutSec, defaults.SessionAffinityTimeoutMaxFallback)
+			svcInfo.SessionAffinityTimeoutSec = defaults.SessionAffinityTimeoutMaxFallback
 		}
 	}
 

noironetworks/cilium-net

commit 4a78bed167feeed63eba65d7434fadde88a650c4

fix: SetLbAlg and SetSessionAffinityTimeoutSec
This corrects the implementation of SetLbAlg() and
SetSessionAffinityTimeoutSec(), adds GetSessionAffinityTimeoutSec()
for completeness, and also adds a test for all these.

Fixes: 1b2bed77dad58e1c98d801762b79262a40a3bf2c
Signed-off-by: Damian Sawicki <dsawicki@google.com>

pkg/maps/lbmap/ipv4.go
@@ -325,11 +325,25 @@ func (s *Service4Value) GetFlags() uint16 {
 	return (uint16(s.Flags2) << 8) | uint16(s.Flags)
 }
 
-func (s *Service4Value) SetSessionAffinityTimeoutSec(t uint32) {
+const (
+	sessionAffinityMask uint32 = 0x00ff_ffff
+	lbAlgMask           uint32 = 0xff00_0000
+)
+
+func (s *Service4Value) SetSessionAffinityTimeoutSec(t uint32) error {
 	// Go doesn't support union types, so we use BackendID to access the
 	// lb4_service.affinity_timeout field. Also, for the master entry the
-	// LB algorithm was set beforehand, so we need to binary OR here.
-	s.BackendID |= t
+	// LB algorithm can be set independently, so we need to preseve the
+	// first 8 bits and only assign to the latter 24 bits.
+	if t > sessionAffinityMask {
+		return fmt.Errorf("session affinity timeout %d does not fit into 24 bits (is larger than 16777215)", t)
+	}
+	s.BackendID = (s.BackendID & lbAlgMask) + (t & sessionAffinityMask)
+	return nil
+}
+
+func (s *Service4Value) GetSessionAffinityTimeoutSec() uint32 {
+	return s.BackendID & sessionAffinityMask
 }
 
 func (s *Service4Value) SetL7LBProxyPort(port uint16) {
@@ -345,12 +359,14 @@ func (s *Service4Value) GetBackendID() loadbalancer.BackendID {
 	return loadbalancer.BackendID(s.BackendID)
 }
 
-func (s *Service4Value) GetLbAlg() uint8 {
-	return uint8(uint32(s.BackendID) >> 24)
+func (s *Service4Value) GetLbAlg() loadbalancer.SVCLoadBalancingAlgorithm {
+	return loadbalancer.SVCLoadBalancingAlgorithm(uint8(uint32(s.BackendID) >> 24))
 }
 
-func (s *Service4Value) SetLbAlg(lb uint8) {
-	s.BackendID = uint32(lb) << 24
+func (s *Service4Value) SetLbAlg(lb loadbalancer.SVCLoadBalancingAlgorithm) {
+	// SessionAffinityTimeoutSec can be set independently on the latter 24 bits,
+	// so we only modify the first 8 bits.
+	s.BackendID = uint32(lb)<<24 + (s.BackendID & sessionAffinityMask)
 }
 
 func (s *Service4Value) ToNetwork() ServiceValue {

pkg/maps/lbmap/ipv6.go
@@ -219,21 +219,30 @@ func (s *Service6Value) SetFlags(flags uint16) {
 	s.Flags2 = uint8(flags >> 8)
 }
 
-func (s *Service6Value) GetLbAlg() uint8 {
-	return uint8(uint32(s.BackendID) >> 24)
+func (s *Service6Value) GetLbAlg() loadbalancer.SVCLoadBalancingAlgorithm {
+	return loadbalancer.SVCLoadBalancingAlgorithm(uint8(uint32(s.BackendID) >> 24))
 }
 
-func (s *Service6Value) SetLbAlg(lb uint8) {
-	s.BackendID = uint32(lb) << 24
+func (s *Service6Value) SetLbAlg(lb loadbalancer.SVCLoadBalancingAlgorithm) {
+	// See (* Service6Value).SetLbAlg() for comment
+	s.BackendID = uint32(lb)<<24 + (s.BackendID & sessionAffinityMask)
 }
 
 func (s *Service6Value) GetFlags() uint16 {
 	return (uint16(s.Flags2) << 8) | uint16(s.Flags)
 }
 
-func (s *Service6Value) SetSessionAffinityTimeoutSec(t uint32) {
+func (s *Service6Value) SetSessionAffinityTimeoutSec(t uint32) error {
 	// See (* Service4Value).SetSessionAffinityTimeoutSec() for comment
-	s.BackendID |= t
+	if t > sessionAffinityMask {
+		return fmt.Errorf("session affinity timeout %d does not fit into 24 bits (is larger than 16777215)", t)
+	}
+	s.BackendID = (s.BackendID & lbAlgMask) + (t & sessionAffinityMask)
+	return nil
+}
+
+func (s *Service6Value) GetSessionAffinityTimeoutSec() uint32 {
+	return s.BackendID & sessionAffinityMask
 }
 
 func (s *Service6Value) SetL7LBProxyPort(port uint16) {

pkg/maps/lbmap/lbmap.go
@@ -615,7 +615,7 @@ func updateMasterService(fe ServiceKey, v ServiceValue, activeBackends, quaranti
 	v.SetCount(activeBackends)
 	v.SetQCount(quarantinedBackends)
 	v.SetRevNat(revNATID)
-	v.SetLbAlg(uint8(loadBalancingAlgorithm))
+	v.SetLbAlg(loadBalancingAlgorithm)
 	flag := loadbalancer.NewSvcFlag(&loadbalancer.SvcFlagParam{
 		SvcType:          svcType,
 		SvcFwdModeDSR:    svcForwardingMode == loadbalancer.SVCForwardingModeDSR,
@@ -631,7 +631,10 @@ func updateMasterService(fe ServiceKey, v ServiceValue, activeBackends, quaranti
 	})
 	v.SetFlags(flag.UInt16())
 	if sessionAffinity {
-		v.SetSessionAffinityTimeoutSec(sessionAffinityTimeoutSec)
+		if err := v.SetSessionAffinityTimeoutSec(sessionAffinityTimeoutSec); err != nil {
+			log.Warn("Failure in updateMasterService due to error from SetSessionAffinityTimeoutSec", logfields.Error, err)
+			return err
+		}
 	}
 	if l7lbProxyPort != 0 {
 		v.SetL7LBProxyPort(l7lbProxyPort)

pkg/maps/lbmap/types.go
@@ -87,7 +87,10 @@ type ServiceValue interface {
 	GetFlags() uint16
 
 	// Set timeout for sessionAffinity=clientIP
-	SetSessionAffinityTimeoutSec(t uint32)
+	SetSessionAffinityTimeoutSec(t uint32) error
+
+	// Get timeout for sessionAffinity=clientIP
+	GetSessionAffinityTimeoutSec() uint32
 
 	// Set proxy port for l7 loadbalancer services
 	SetL7LBProxyPort(port uint16)
@@ -108,10 +111,10 @@ type ServiceValue interface {
 	ToHost() ServiceValue
 
 	// Set LoadBalancing Algorithm for Service
-	SetLbAlg(uint8)
+	SetLbAlg(loadbalancer.SVCLoadBalancingAlgorithm)
 
 	// Get LoadBalancing Algorithm for Service
-	GetLbAlg() uint8
+	GetLbAlg() loadbalancer.SVCLoadBalancingAlgorithm
 }
 
 // BackendKey is the interface describing protocol independent backend key.

pkg/maps/lbmap/ipv4v6_test.go
@@ -1,0 +1,69 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package lbmap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/pkg/loadbalancer"
+)
+
+func TestLbAlgAndSessionAffinityTimeout(t *testing.T) {
+	var (
+		alg1        loadbalancer.SVCLoadBalancingAlgorithm = 1
+		alg2        loadbalancer.SVCLoadBalancingAlgorithm = 2
+		time1       uint32                                 = 8
+		time2       uint32                                 = 16
+		invalidTime uint32                                 = 1 << 24
+	)
+	type testCase struct {
+		desc string
+		svc  ServiceValue
+	}
+	testCases := []testCase{
+		{"v4", &Service4Value{}},
+		{"v6", &Service6Value{}},
+	}
+	type testScenario struct {
+		desc   string
+		action func(*testing.T, ServiceValue)
+	}
+	testScenarios := []testScenario{
+		{
+			"GetLbAlg",
+			func(t *testing.T, svc ServiceValue) {
+				svc.SetLbAlg(alg1) // This is to verify that earlier writes are not visible if overridden later.
+				svc.SetLbAlg(alg2)
+				svc.SetSessionAffinityTimeoutSec(time1) // This is to verify that writing to different bits of the same 32-bit variable does not affect the LbAlg bits.
+				require.Equal(t, alg2, svc.GetLbAlg())
+			},
+		},
+		{
+			"GetSessionAffinityTimeoutSec",
+			func(t *testing.T, svc ServiceValue) {
+				svc.SetSessionAffinityTimeoutSec(time1) // This is to verify that earlier writes are not visible if overridden later.
+				svc.SetSessionAffinityTimeoutSec(time2)
+				svc.SetLbAlg(alg2) // This is to verify that writing to different bits of the same 32-bit variable does not affect the SessionAffinityTimeoutSec bits.
+				require.Equal(t, time2, svc.GetSessionAffinityTimeoutSec())
+			},
+		},
+		{
+			"Too large SessionAffinityTimeoutSec",
+			func(t *testing.T, svc ServiceValue) {
+				require.Error(t, svc.SetSessionAffinityTimeoutSec(invalidTime))
+			},
+		},
+	}
+	for _, tc := range testCases {
+		for _, ts := range testScenarios {
+			t.Run(tc.desc+"/"+ts.desc, func(t *testing.T) {
+				svc := tc.svc.New().(ServiceValue)
+				ts.action(t, svc)
+			})
+		}
+
+	}
+}